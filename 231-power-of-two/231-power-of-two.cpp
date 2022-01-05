39d75f8b926e8b4228a509f38d05aa1b77cad376
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-01-05 09:31:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (21.84%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
-            return 0;
-        int k = log2(n);
-        if(pow(2, k)==n)
-            return 1;
-        else
-            return 0;
+            return false;
+        return (not (n & (n-1)));
     }
 };
