5e5fa1104d200af4e76e14f3e39a8fe177f66b2e
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-01-05 09:42:57 +0530

    Time: 3 ms (34.15%), Space: 6 MB (24.81%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -3,15 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n<=0)
             return false;
-        int p=0;
-        int sbc = __builtin_popcount(n);
-        while(n){
-            p++;
-            n = n>>1;
-        }
-        
-        if(p&1 and sbc==1)
-            return true;
-        return false;
+        int val = log2(n)/2;
+        return (pow(4, val)==n);
     }
 };
