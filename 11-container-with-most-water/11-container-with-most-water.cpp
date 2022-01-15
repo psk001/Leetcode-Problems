9723bea424e31a48522314e7479634e145af7906
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-01-16 01:26:51 +0530

    Time: 181 ms (10.96%), Space: 59.1 MB (37.99%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -0,0 +1,25 @@
+// brute force solution
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+  
+        int res=-1;
+        int i=0, j=height.size()-1;
+        
+        while(i<j){
+            int curCap = min(height[i], height[j])*(j-i);
+            res=max(res, curCap);
+            // cout << "res: " << res << " curCap: " << curCap << endl;
+            if(height[j]<height[i])
+                j--;
+            
+            else
+                i++;
+        }
+        
+        return res;        
+    }
+};
+
+    
