712421a1b5040450538fad79ba407a0961fee385
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-01-18 10:37:28 +0530

    Time: 38 ms (8.75%), Space: 20.4 MB (20.95%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& fd, int n) {
-        int res=0;
-        
-        if(fd.size()==1 and fd[0]==0)
-            return (res<=n);
-        
-        else if(fd.size()==1 and fd[0]==1)
-            return (n==0);
-        
-        if((fd[0]==0 and fd[1]==0) ){
-                res++;
-                fd[0]=1;
-        }
-        
-        if(fd.size()>2 and fd[fd.size()-1]==0 and fd[fd.size()-2]==0){
-                res++;
-                fd[fd.size()-1]=1;
-        }
-        
-        for(int i=1; i<fd.size()-1; i++){          
+        int res=0, i=0;
+
+        while(i<fd.size()){
             
-            if(fd[i]==0 and fd[i-1]==0 and fd[i+1]==0){
-                fd[i]=1;
-                res++;
-            }
-            cout << "i: " << i << " res: " << res << endl;
+            if( fd[i]==0 and (i==0 or fd[i-1]==0) and (i==fd.size()-1 or fd[i+1]==0) )
+                fd[i]=1, res++;
+            
+            if(res>=n)
+                return true;
+            
+            i++;
         }
         
-        return (res>=n);
+        return false;
     }
 };
