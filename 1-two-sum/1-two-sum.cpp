cf04f733c6c1b1c2de89969e71a9852c7665f4fd
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-01-25 10:38:07 +0530

    Time: 20 ms (55.11%), Space: 13.3 MB (5.70%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        map<int, int> col;
+        
+        for(int i=0; i<n; i++)
+            col[nums[i]]=i;
+        
+        vector<int> p(2);
+        
+        for(int i=0; i<n; i++)
+            if(col[target-nums[i]] > 0 and col[target-nums[i]] != i){
+                p[0]=i, p[1]=col[target-nums[i]];
+                break;
+            }
+
+         return p;
+    }
+};
