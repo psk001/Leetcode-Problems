1e78e17ba820a8e5529a18e2b8b9662c2d74be00
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-01-26 01:46:14 +0530

    Time: 137 ms (25.44%), Space: 48.4 MB (49.78%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int step=0;
+        int reqSteps=0;
         
         for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i]>=step)
-                step=0;
-            step++;
+            if(nums[i]>=reqSteps)
+                reqSteps=0;
+            
+            reqSteps++;
         }
         
-        return step==1;
-    }
+        return (reqSteps==1);
+    } 
     
 
 };
