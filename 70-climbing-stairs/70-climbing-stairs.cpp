f12b940fec654677a16a80f75780074f0046da9a
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-02-01 11:48:20 +0530

    Time: 3 ms (19.55%), Space: 6.1 MB (36.71%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int> col{0,1,2};
-        
-        for(int i=3; i<=n; i++)
-            col.push_back(col[i-1]+col[i-2]);
-        
-        return col[n];
+    int climbStairs(int n) {    
+        vector<int> res(n, -1);
+        return numSteps(res, 0, n);
     }
+    
+    int numSteps(vector<int>& res, int curr, int n){
+        if(curr==n)
+            return 1;
+        if(curr>n)
+            return 0;
+        
+        if(res[curr] != -1)
+            return res[curr];
+        
+        res[curr]=numSteps(res, curr+1, n)+numSteps(res, curr+2, n);
+        
+        return res[curr];
+    }
+    
 };
