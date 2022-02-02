1da95f132600097ea7a4ce1c038ebdc3da06eba0
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-02-02 22:47:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.71%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,10 +5,10 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l=1, r=n, m;
-        while(l<=r){
+        while(l<r){
             int m=l+(r-l)/2;
             if(isBadVersion(m))
-                r=m-1;            
+                r=m;            
             else
                 l=m+1;    
         }
