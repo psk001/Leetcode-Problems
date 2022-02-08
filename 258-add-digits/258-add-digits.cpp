b0b5023f1c027cd45d0129b779636e93e214c7c5
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-02-09 00:06:45 +0530

    Time: 4 ms (43.34%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int addDigits(int n) {
         if(n==0) return 0;
-        if(n%9==0)return 9;
-        else return n%9;
+       // if(n%9==0)return 9;
+        else return 1+(n-1)%9;
     }
 };
