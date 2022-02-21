f5ee53580a8254849535d96a47ddbb973a8d1d5b
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-02-21 09:32:24 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++)
-            for(int j=i+1; j<nums.size(); j++)   // bubble sort 
-                if(nums[i]>nums[j])
-                    swap(nums[i], nums[j]);
+        if(nums.size()<2)
+            return;
+        int r=nums.size()-1, l=0;
+        int c=0;
+        vector<int> res(r+1);
+    //    cout << "initial c: " << c << endl;
+        for(auto i: nums)
+            if(i==0 or i==2)
+                c++;
+        
+        for(int i=0; i<nums.size(); i++){
+            
+            if(nums[i]==0){
+                res[l]=0;
+                l++;              
+            }
+            
+            else if(nums[i]==2){
+                res[r]=2;
+                r--;                
+            }
+            
+        }
+        for(int i=l; i<=r; i++)
+            res[i]=1;
+        // for(auto i: nums)
+        //     cout << i << " ";
+        // cout << endl;
+        nums=res;
         
         return;
     }
