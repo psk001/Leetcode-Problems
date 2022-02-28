c030551a4c65b85edec508dcaa6a3a1b35e27093
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-02-28 18:30:10 +0530

    Time: 3 ms (56.33%), Space: 7.8 MB (59.49%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        
+        if(nums.size()==2)
+            return max(nums[0], nums[1]);        
+        vector<int> v1(nums.begin(), nums.end()-1);
+        vector<int> v2(nums.begin()+1, nums.end());
+        
+        return max(robb(v1), robb(v2));
+    }
+    
+    int robb(vector<int>& nums){
+        if(nums.size()==1)
+            return nums[0];
+        
+        if(nums.size()==2)
+            return max(nums[0], nums[1]);
+        
+        vector<int> dp(nums.size(), -1);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0], nums[1]);
+        
+        for(int i=2; i<nums.size(); i++)
+            dp[i]=max(nums[i]+dp[i-2], dp[i-1]);
+        
+        return dp[dp.size()-1];
+    }
+};
