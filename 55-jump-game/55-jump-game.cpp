e40c56549ac8833072cb27bd59b4e8e6abab7954
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-03-01 16:04:35 +0530

    Time: 59 ms (89.82%), Space: 48.7 MB (20.12%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int reqSteps=0;
-        
-        for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i]>=reqSteps)
-                reqSteps=0;
-            
-            reqSteps++;
-        }
-        
-        if(reqSteps==1)
+        if(nums.size()==1 and nums[0]>0)
             return true;
         
-        return false;
-    } 
+        vector<bool> dp(nums.size(), false);
+        
+        int req=0;
+        
+        for(int i=nums.size()-1; i>= 0; i--){
+            if(req<=nums[i])
+                req=0;
+            
+            req++;
+        }
+        
+        return (req==1);
+        
+    }
     
-
+//     bool jump(vector<int>& nums, int idx, int val){
+        
+//     }
 };
