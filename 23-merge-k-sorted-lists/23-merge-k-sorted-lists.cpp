2e1cb382906ad82b18cd2220c8a49ef35d3e557e
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-03-07 06:10:38 +0530

    Time: 240 ms (20.82%), Space: 13.5 MB (50.13%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -35,15 +35,12 @@ public:
         if(not list2)
             return list1;
         
-        
         if(list1->val < list2->val){
-            list1->next=mergeTwoLists(list1->next, list2);  
+            list1->next=mergeTwoLists(list1->next, list2);
             return list1;
         }
         
-        
         list2->next=mergeTwoLists(list1, list2->next);
         return list2;
-
     }    
 };
