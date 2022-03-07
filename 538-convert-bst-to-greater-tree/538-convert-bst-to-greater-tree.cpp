39369f212ab2fa59554f948a323668e61853da8d
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-03-08 02:48:06 +0530

    Time: 67 ms (31.02%), Space: 35.6 MB (5.38%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> val;
+    map<int, int> gt;
+    
+    TreeNode* convertBST(TreeNode* root) {
+        if(not root)
+            return root;
+        
+        iterate(root);
+        sort(val.begin(), val.end());
+        
+        gt[val[val.size()-1]]=val[val.size()-1];
+        
+        for(int i=val.size()-2; i>=0; i--){
+            int k=val[i]+val[i+1];
+            gt[val[i]]=k;
+            val[i]=k;
+        }
+        
+        grt(root);
+        
+        return root;
+    }
+    
+    void iterate(TreeNode* root){
+        if(not root)
+            return;
+        
+        val.push_back(root->val);
+        
+        iterate(root->left);
+        iterate(root->right);
+    }
+    
+    void grt(TreeNode* root){
+        if(not root)
+            return;
+        
+        root->val = gt[root->val];
+        
+        grt(root->left);
+        grt(root->right);        
+    }
+    
+};
