972cb223c60abcc4c57cc269084c1b34c1326175
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-03-16 00:59:05 +0530

    Time: 117 ms (52.64%), Space: 14.5 MB (51.29%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int n) {
+        vector<int> dp(++n);
+        
+        dp[0]=0;
+        
+        sort(coins.begin(), coins.end());
+        
+        for(int i=1; i<n; i++){
+            dp[i] = INT_MAX;
+            for(int c: coins){
+                if(i-c<0)
+                    break;
+                
+                if(dp[i-c] != INT_MAX)
+                    dp[i]=min(dp[i], 1+dp[i-c]);
+                
+            }
+        }
+        
+        return (dp[--n]==INT_MAX ? -1 : dp[n]);
+        
+        
+    }
+};
