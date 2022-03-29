38a3c7fca282697b4ea7059193714db8b5a05878
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-03-29 13:14:47 +0530

    Time: 753 ms (5.00%), Space: 40.5 MB (5.01%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int countQuadruplets(vector<int>& arr) {
-        int res=0, n=arr.size();
-        
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
+    int countQuadruplets(vector<int>& nums) {
+        int res=0;
+        int n=nums.size();
+        set<vector<int> > t;
+        for(int i=0; i<n; i++)
+            for(int j=i+1; j<n; j++)
                 for(int k=j+1; k<n; k++)
                     for(int l=k+1; l<n; l++)
-                        if(arr[i]+arr[j]+arr[k]==arr[l])
-                            res++;
-            }
-        }
+                        if(nums[i]+nums[j]+nums[k]==nums[l]){
+                            vector<int>p({i, j, k, l});
+                            sort(p.begin(), p.end());
+                            t.insert(p);
+                            // cout << i << " " << j << " " <<  k<< " " << l << endl;
+                        }
+        res=t.size();
         
         return res;
     }
