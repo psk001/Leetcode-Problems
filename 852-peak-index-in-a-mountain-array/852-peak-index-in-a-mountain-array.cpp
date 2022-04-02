dc18566f2d4516cac06140100fa3dc517c6fb498
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-04-02 08:10:23 +0530

    Time: 21 ms (15.33%), Space: 11.5 MB (89.70%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -3,18 +3,18 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int l=0, r=arr.size()-1;
         
-        while(l<=r){
+        while(l<r){
             int mid=l+(r-l)/2;
-            if(arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1])
-                return mid;
+            // if(arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1])
+            //     return mid;
             
             if(arr[mid]<arr[mid+1])
                 l=mid+1;
             else
-                r=mid-1;
+                r=mid;
             // cout << "l: " << l << " r: " << r << " mid: " << mid << endl;
         }
         
-        return -1;
+        return l;
     }
 };
