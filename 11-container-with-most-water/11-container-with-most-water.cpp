ec11bf726c7a3ab46eb9c766d362f13b8a3eb54c
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-04-05 08:18:29 +0530

    Time: 123 ms (50.44%), Space: 59.1 MB (38.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,25 +1,17 @@
-// brute force solution
-
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-  
-        int res=-1;
-        int i=0, j=height.size()-1;
-        
-        while(i<j){
-           // int curCap = min(height[i], height[j])*(j-i);
-            res=max(res, min(height[i], height[j])*(j-i));
+    int maxArea(vector<int>& h) {
+        int l=0, r=h.size()-1;
+        int res=INT_MIN;
+        while(l<r){
+            res=max(res, min(h[l], h[r])*(r-l));
             
-            if(height[j]<height[i])
-                j--;
-            
+            if(h[l]<=h[r])
+                l++;
             else
-                i++;
+                r--;
         }
         
-        return res;        
+        return res;
     }
 };
-
-    
