c64a0d07e56631b33712ab64b743d1f1db3bb12e
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-06-07 08:18:57 +0530

    Time: 4 ms (49.07%), Space: 9.1 MB (72.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(m==0 and n==0)
-            return;
+        int i=m-1, j=n-1, k=m+n-1;
         
-        if(n==0)
-            return;
-        
-        if(m==0){
-            nums1=nums2;
-            return;
-        }
-        
-        int i=0, j=0;
-        
-        while(i<m+n and j<n){
+        while(i>=0 and j>=0){
             if(nums1[i]>nums2[j]){
-                int prev=m+n-1;
-                while(prev>=i and prev>0){
-                    nums1[prev]=nums1[prev-1];
-                    prev--;                   
-                }
-                nums1[i]=nums2[j];
-                j++;
-            } 
-            
-            i++;
-            // cout << "i: " << i << " j: " << j << endl;
-            // for(auto i: nums1)
-            //     cout << i << " ";
-            // cout << endl;
-        }
-        
-        if(j!=n){
-            i-=n-j;
-            while(j!=n){
-                nums1[i]=nums2[j];
-                i++, j++;
+                nums1[k]=nums1[i];
+                k--, i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                k--, j--;
             }
         }
-        return;
+        
+        while(j>=0){
+            nums1[k]=nums2[j];
+            k--, j--;
+        }
+    
     }
 };
