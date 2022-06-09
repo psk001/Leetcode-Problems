e3014ac86666538e9d5005dea236b4535ea63320
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-06-09 09:51:18 +0530

    Time: 35 ms (8.99%), Space: 10.4 MB (6.53%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,37 +1,33 @@
 class Solution {
+    int m, n;
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        if(matrix.size()==1)
-            return matrix[0][0];
+        m=matrix.size(), n=matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 101));
         
-        int n=matrix.size();
-        vector<vector<int> > dp(n+1, vector<int> (n+1, -1));
         int res=INT_MAX;
-        
         for(int i=0; i<n; i++)
-            res = min(res, fps(matrix, dp, 0, i, n));
+            res=min(res, getSum(matrix, dp, 0, i));
         
         return res;
-        
     }
     
-    int fps(vector<vector<int>>& matrix, vector<vector<int>>& dp, int crow, int ccol, int n){
-        if(ccol>=n || ccol<0)
-            return INT_MAX;
-        
-        if(crow>=n)
+    int getSum(vector<vector<int>>& matrix, vector<vector<int>>& dp, int crow, int ccol){
+        if(crow>=m)
             return 0;
         
-        if(dp[crow][ccol] != -1)
+        if(ccol<0 or ccol>=n)
+            return INT_MAX;
+        
+        if(dp[crow][ccol]!=101)
             return dp[crow][ccol];
         
-        int pcol = fps(matrix, dp, crow+1, ccol-1, n);
-        int scol = fps(matrix, dp, crow+1, ccol, n);
-        int ncol = fps(matrix, dp, crow+1, ccol+1, n);
+        int nrpc= getSum(matrix, dp, crow+1, ccol-1);
+        int nrsc= getSum(matrix, dp, crow+1, ccol);
+        int nrnc= getSum(matrix, dp, crow+1, ccol+1);
         
-        dp[crow][ccol] = matrix[crow][ccol] + min(pcol, min(scol, ncol));
+        dp[crow][ccol]= matrix[crow][ccol]+ min(nrpc, min(nrsc, nrnc));
         
-        return dp[crow][ccol];        
+        return dp[crow][ccol];
     }
-    
 };
