772e95d58c31d030cbab9a4634191ba2fd34b971
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-07-28 11:13:54 +0530

    Time: 9 ms (78.39%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        vector<int> st(26, 0);
+        for(auto i: s)
+            st[i-'a']++;
         
-        return s==t;
+        for(auto i: t)
+            st[i-'a']--;
+        
+        for(auto i: st)
+            if(i!=0)
+                return false;
+        
+        return true;
     }
+        
 };
