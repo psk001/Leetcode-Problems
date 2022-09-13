489ec3000369be2157f54051d5b6cbb52838835e
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-09-13 19:36:09 +0530

    Time: 3 ms (55.06%), Space: 7.9 MB (25.89%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==1)
-            return nums[0];
-        
-        vector<int> dp(nums.size(), -1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0], nums[1]);
-        
-        for(int i=2; i<nums.size(); i++){
-            dp[i] = max(nums[i]+dp[i-2], dp[i-1]);
-        }
-        
-        return dp[dp.size()-1];
+        vector<int> col(nums.size(), -1);
+        return mon(nums, col, 0);
     }
     
+    int mon(vector<int>& nums, vector<int>& col, int idx){
+        if(idx>=nums.size()){
+            return 0;
+        }
+        
+        if(col[idx]!=-1)
+            return col[idx];
+        
+        int tk= nums[idx]+mon(nums, col, idx+2);
+        int nt= mon(nums, col, idx+1);
+        col[idx]= max(tk, nt);
+        return col[idx];
+    }
 };
