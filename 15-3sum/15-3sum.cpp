58efd947c034e202ad6e1eebd4bb20533847df43
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-10-09 14:55:56 +0530

    Time: 112 ms (81.68%), Space: 19.9 MB (63.28%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        if(nums.size()<3)
-            return {};
-        
         sort(nums.begin(), nums.end());
-        
         if(nums[0]>0)
             return {};
         
+        int n= nums.size();
+                
         vector<vector<int>> res;
-
         
-        for(int i=0; i<nums.size(); i++){
-            
-            if(nums[i]>0)
+        for(int i=0; i<n; i++){
+            if(nums[i]>0 or i==n-1)
                 break;
             
-            if(i>0 and nums[i]==nums[i-1])
-                continue;
+           if(i>0 and nums[i]==nums[i-1])
+               continue;
             
-            int l=i+1, r=nums.size()-1;
+            int l= i+1, r= n-1;
             
             while(l<r){
-                int sum=nums[i]+nums[l]+nums[r];
+                int val= nums[l]+nums[i]+nums[r];
                 
-                if(sum<0)
+                if(val==0){
+                    res.push_back({nums[l], nums[i], nums[r]});
+                
+                    int clow= nums[l], chigh= nums[r];
+                
+                    while(l<n and clow==nums[l])
+                        l++;
+                
+                    while(r>0 and chigh==nums[r])
+                        r--;                    
+                }
+                
+                else if (val<0)
                     l++;
-                else if(sum>0)
+                else 
                     r--;
                 
-                else{
-                    res.push_back({nums[i], nums[l], nums[r]});
-                    
-                    int clow=nums[l], chigh=nums[r];
-                    
-                    while(l<r and clow==nums[l])
-                        l++;
-                    
-                    while(l<r and chigh==nums[r])
-                        r--;
-                }
             }
+                
         }
-        
-        
+                
         return res;
     }
-    
 };
