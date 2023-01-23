470278dfae16333631347c5cc191ef2b63f1b65d
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-01-23 23:16:33 +0530

    Time: 219 ms (59.13%), Space: 68.8 MB (42.03%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==1 and trust.size()==0)
+            return 1;
+        
+        if(trust.size()==0)
+            return -1;        
+        
+        int col[n+1][2];
+        memset(col, 0, sizeof(int)*(n+1)*2);
+        
+        for(auto i: trust){
+            col[i[0]][0]++;
+            col[i[1]][1]++;
+        }
+              
+        for(int i=1; i<=n; i++){
+            if(col[i][1]==n-1 and col[i][0]==0)
+                return i;
+        }
+        return -1;
+    }
+};
