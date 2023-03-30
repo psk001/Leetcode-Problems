15015d64d2d0a56426a1dbd067a4da76e4949514
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-03-30 18:00:38 +0530

    Time: 24 ms (37.52%), Space: 13.4 MB (43.49%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            y = (-1)*x
+            p = 0
+            while(y!=0):
+                rem = y%10
+                y = y/10
+                p = p*10+rem
+                
+            p = (-1)*p
+            
+            if p < (-1)*(2**31):
+                return 0
+            else:
+                return p
+            
+        else:
+            p = 0
+            while(x!=0):
+                rem = x%10
+                x = x/10
+                p = p*10+rem
+            
+            if p > 2**31 -1:
+                return 0
+            else:
+                return p            
+            
+        
