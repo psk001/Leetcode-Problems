9dd4d12045169d9318ca07162577f0a8e104972d
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-05-23 11:24:26 +0530

    Time: 41 ms (32.81%), Space: 19.7 MB (99.56%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -5,26 +5,22 @@ public:
     
     KthLargest(int k, vector<int>& nums) {
         r=k;
-        // cout << 1 << endl;
-        
+               
         sort(nums.begin(),  nums.end(), greater<int>());
-        // cout << 2 << endl;                    
-        
+               
         if(nums.size()>0)
             for(int i=0; i<k and i<nums.size(); i++)
                 gq.push(nums[i]);
-        // cout << 3 << endl;
+      
     }
     
     int add(int val) {
-        // cout << 4 << endl;
+    
         gq.push(val);
         
-        // cout << 5 << endl;
         if(gq.size()>r)
             gq.pop();
         
-        // cout << 6 << endl;
         return gq.top();
         
     }
