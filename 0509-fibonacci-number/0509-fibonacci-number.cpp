f4d51447558748721daf648703fbd29fa971da53
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-05-23 21:57:05 +0530

    Time: 3 ms (69.24%), Space: 5.9 MB (94.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,13 +4,17 @@ public:
         if(n<=1)
             return n;
         
-        vector<int> st(n+1, 0);
         
-        st[0]=0, st[1]=1;
         
-        for(int i=2; i<=n; i++)
-            st[i]= st[i-1]+st[i-2];
+        int a=0, b=1, res;
         
-        return st[n];
+        for(int i=2; i<=n; i++){
+            res=a+b;
+            a=b;
+            b=res;
+        }
+            
+        
+        return res;
     }
 };
