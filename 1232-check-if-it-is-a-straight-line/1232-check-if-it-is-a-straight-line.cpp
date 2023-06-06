536c224d2270944eebeeb23d365ab5e4e306aca0
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-06-06 10:51:15 +0530

    Time: 11 ms (57.17%), Space: 10.1 MB (28.99%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -9,31 +9,28 @@ public:
         if(cd[0][0]==cd[n-1][0]){
             
             int px= cd[0][0];
-            cout << "from x: " << " px: " << px << endl;
-            for(auto p: cd){
-                cout << p[0] << " ";
-                if(p[0]!=px){
-                    cout <<"returning from 16";
+           
+            for(auto p: cd)
+                if(p[0]!=px)
                     return false;
-                }
-            }
+            
             return true;
         }
         
         double m= (double) (cd[n-1][1]-cd[0][1])/ (cd[n-1][0]-cd[0][0]);
         
-        if(m==0) {
-            int py= cd[0][1];
-            cout << "from y";
-            for(auto p: cd){
-                cout << p[1] << " ";
-                if(p[1]!=py){
-                    cout <<"returning from 30";
-                    return false;   
-                }
-            }
-            return true;
-        }
+//         if(m==0) {
+//             int py= cd[0][1];
+            
+//             for(auto p: cd){
+//                 cout << p[1] << " ";
+//                 if(p[1]!=py){
+//                     cout <<"returning from 30";
+//                     return false;   
+//                 }
+//             }
+//             return true;
+//         }
         
         for(int i=1; i<n; i++){
             double cm= (double) (cd[i][1]-cd[i-1][1])/ (cd[i][0]-cd[i-1][0]);
