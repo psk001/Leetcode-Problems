446364fbcde283c81e1e696d46d0210f9b02c049
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-06-06 10:50:08 +0530

    Time: 11 ms (57.17%), Space: 10 MB (91.79%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& cd) {
+        
+        sort(cd.begin(), cd.end(), cmp);
+                
+        int n= cd.size();
+        
+        if(cd[0][0]==cd[n-1][0]){
+            
+            int px= cd[0][0];
+            cout << "from x: " << " px: " << px << endl;
+            for(auto p: cd){
+                cout << p[0] << " ";
+                if(p[0]!=px){
+                    cout <<"returning from 16";
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        double m= (double) (cd[n-1][1]-cd[0][1])/ (cd[n-1][0]-cd[0][0]);
+        
+        if(m==0) {
+            int py= cd[0][1];
+            cout << "from y";
+            for(auto p: cd){
+                cout << p[1] << " ";
+                if(p[1]!=py){
+                    cout <<"returning from 30";
+                    return false;   
+                }
+            }
+            return true;
+        }
+        
+        for(int i=1; i<n; i++){
+            double cm= (double) (cd[i][1]-cd[i-1][1])/ (cd[i][0]-cd[i-1][0]);
+            
+            if(cm!=m){
+                cout <<"returning from 40";
+                return false;   
+            }
+        }
+        
+        
+        return true;
+        
+    }
+    
+    bool static cmp(vector<int>& a, vector<int>& b){
+        return a[0]<b[0];
+    }
+};
