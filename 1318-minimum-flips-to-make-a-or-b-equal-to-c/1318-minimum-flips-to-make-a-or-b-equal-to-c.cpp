d186a62f8866a2c295a0a5aed904ee172efd06b5
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-06-07 07:27:34 +0530

    Time: 3 ms (40.05%), Space: 5.9 MB (93.33%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,23 +4,16 @@ public:
         int res=0;
                 
         while(a or b or c){
-            int al= a&1;
-            int bl= b&1;
-            int cl= c&1;
-            
-            // cout << al << " " << bl <<" " << cl << endl;
-            // cout << "al | bl: " << (al | bl) << " cl: " << cl << endl;
+            int al= a&1, bl= b&1, cl= c&1;
+                    
             if ((al | bl) != cl){
-                if(cl==0 and al==bl){
+                if(cl==0 and al==bl)
                     res+=2;
-                }else{
+                else
                     res+=1;
-                }
             }
-            // cout << "res: " << res << endl;
-            a=a>>1;
-            b=b>>1;
-            c=c>>1;
+            
+            a=a>>1, b=b>>1, c=c>>1;
         }
         
         return res;
