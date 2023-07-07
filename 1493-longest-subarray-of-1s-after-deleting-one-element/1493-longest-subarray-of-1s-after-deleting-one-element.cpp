d8cc06ba85b5b4643550329914f87800fbceb059
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-07-08 02:39:53 +0530

    Time: 48 ms (47.85%), Space: 36.5 MB (18.86%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,39 +2,18 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int n= nums.size();
-        vector<int> zdx;
+        
+        int zc= 0, res=0, start=0;
         
         for(int i=0; i<n; i++){
-            if(nums[i]==0)
-                zdx.push_back(i);
-        }
-        
-        int m= zdx.size();
-        
-        if(m==0)
-            return n-1;
-        
-        if(m<2 || m==n){
-            // cout << "m: " << m << " returning\n";
-            return n-m;
-        }
-        
-        int res=INT_MIN;
-        
-        for(int i=0; i<m; i++){
-            if(zdx[i]==0 || i==0){   // handle first idx
-                res=max(res, zdx[1]-1);
-            }else if(i==m-1){
-                res=max(res, n-1-zdx[i-1]-1);
-            }else{
-                int nz= zdx[i+1];
-                int pz= zdx[i-1];
-                int cl= nz-1-pz-1;
-                
-                res= max(res, cl);
+            zc+= (nums[i]==0);
+            
+            while(zc>1){
+                zc-= (nums[start]==0);
+                start++;
             }
             
-            // cout << "i: " << i << "res: " << res << endl;
+            res= max(res, i-start);
         }
         
         return res;
