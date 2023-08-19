dcecffc84cc018eb115d19cb6b4b9e22a4e2e49d
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-08-20 02:53:27 +0530

    Time: 91 ms (44.64%), Space: 39.5 MB (25.51%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -9,11 +9,11 @@ public:
             adj[road[1]].insert(road[0]);
         }
 
-        for (int node1 = 0; node1 < n; ++node1) {
-            for (int node2 = node1 + 1; node2 < n; ++node2) {
+        for (int node1 = 0; node1 < n; node1++) {
+            for (int node2 = node1 + 1; node2 < n; node2++) {
                 int currentRank = adj[node1].size() + adj[node2].size();
                 if (adj[node1].find(node2) != adj[node1].end()) {
-                    --currentRank;
+                    currentRank--;
                 }
               
                 maxRank = max(maxRank, currentRank);
