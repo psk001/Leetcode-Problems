e87f6f489078c3a5a946b6e63685da05e98aea7e
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-09-16 19:01:11 +0530

    Time: 1147 ms (17.61%), Space: 191.9 MB (7.87%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,56 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> result;
+        map<vector<int>, int> m;
         vector<vector<int>> res;
-        int n= nums.size()-1;
         
+        int n= nums.size();
         sort(nums.begin(), nums.end());
         
-        // escaping duplicates from back
-        // while(n>0 and nums[n]==nums[n-1])
-            // n--;
-        
-        // escaping duplicates from front
-        int p=0;
-        // while( p<n-1 and nums[p]==nums[p+1])
-            // p++;
-        
-        
-        for(int i=p; i<n; i++){
-            // cout << "i: " << i << endl;
-            int j=i+1, k=n;
-            
-//             while(j<n-1 and nums[j]==nums[j+1] )
-//                 j++;
-            
-            // cout << "j: " << j;
-            
-//             while( k>0 and nums[k]==nums[k-1])
-//                 k--;
-            
-            // cout << " k: " << k << endl;
-            
+        for(int i=0; i<n; i++){
+            // cout << i << endl;
+            int j= i+1, k= n-1;
             while(j<k){
-                int sum= nums[i]+nums[j]+nums[k];
-                // cout << "sum: " << sum;
-                if(sum>0)
-                    k--;
-            
-                else if(sum<0)
-                    j++;
-                
-                else{
-                    result.insert({nums[i], nums[j], nums[k]});
-                    j++,k--;
+                // cout << j << " " << k << endl;
+                if(nums[i]+nums[j]+nums[k]==0){
+                    m[{nums[i], nums[j], nums[k]}]++;;
+                    j++, k--;
                 }
-                
-                // cout << "\nInside while :  i: " <<  i << " j: " << j << " k: " << k << endl;
+                else if(nums[i]+nums[j]+nums[k]<0)
+                    j++;
+                else
+                    k--;
             }
         }
         
-        for(auto i: result)
-            res.push_back(i);
+        for(auto i: m)
+            res.push_back(i.first);
         
         return res;
     }
