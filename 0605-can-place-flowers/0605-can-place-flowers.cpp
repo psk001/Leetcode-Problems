c027002b3cc17b85235c5e432be0acf013c7f7ac
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-09-16 17:42:16 +0530

    Time: 152 ms (5.82%), Space: 20.6 MB (11.73%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& fd, int n) {
-        int res=0, i=0;
-
-        while(i<fd.size()){
-            
-            if( fd[i]==0 and (i==0 or fd[i-1]==0) and (i==fd.size()-1 or fd[i+1]==0) )
-                fd[i]=1, res++;
-            
-            if(res>=n)
+        if(n==0)
+            return true;
+        
+        int p= fd.size();
+               
+        if(p==1){
+            if(fd[0]==0 and n==1)
                 return true;
-            
-            i++;
+                        
+            if(fd[0]==1)
+                return false;
+           
         }
         
-        return false;
+        for(int i=0; i<p; i++){
+           cout << "big i: " << i << " n " << n << endl;
+
+            if(i==0){
+                if(fd[0]==0 and fd[1]==0)
+                    n--, i++;
+            }
+            
+            else if(i==p-1){
+                if(fd[i]==0 and fd[i-1]==0)
+                    n--, i++;
+            }
+            
+            else if(fd[i]==0 and fd[i-1]==0 and fd[i+1]==0){
+                n--, i++;
+            }
+            
+            cout << "end i: " << i << " n " << n << endl;
+            if(n==0)
+                return true;
+        }
+        
+        return n==0;
+       
     }
 };
+
+
+      // to plant at i, 
+        // fd[i]=0, fd[i-1]=0, fd[i+1]=0;
+        // if i==0, fd[0]=0 and fd[1]=0
+        // if i=p-1 fd[p-1]=0, fd[p=2] =0
+        // once placed, make fd[i+1]=1
+        // n--
+        // final n==0  
+        // 0 0 true
+        // 0 1 true
+        // 1 0 tru
+        // 1 1 true
