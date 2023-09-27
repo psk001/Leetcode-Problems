0444b49795b5a31b7c2eba7c1ad1763f332ad0a9
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-09-27 22:49:02 +0530

    Time: 260 ms (6.72%), Space: 96.1 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, INT_MAX-1);
+        sort(coins.begin(), coins.end(), greater<int>());
+        vector<vector<int>> dp(amount+1, vector<int>(coins.size()+1, -1));
         
-        dp[0]=0;
-        sort(coins.begin(), coins.end());
-        for(int i=1; i<=amount; i++){
-            // cout << " i: " << i << endl;
-            for(auto c: coins){
-                if(c>i)
-                    break;
-                
-                dp[i]= min(dp[i], 1+dp[i-c]);
-            }
-        }
+        int k= ncoin(coins, dp, 0, amount);
         
-        return (dp[amount]==INT_MAX-1? -1: dp[amount]);
+        return (k==INT_MAX-1? -1 : k);
+    }
     
+    int ncoin(vector<int>& c, vector<vector<int>>& dp, int idx, int t){
+      
+        if(t<0)
+            return INT_MAX-1;
+        
+        if(t==0)
+            return 0;
+        
+        if(idx==c.size())
+            return INT_MAX-1;
+        
+        if(dp[t][idx]!=-1)
+            return dp[t][idx];
+    
+        int dt= ncoin(c, dp, idx+1, t);
+            
+        int tk= INT_MAX;
+        if(c[idx]<=t)
+            tk= 1+ncoin(c, dp, idx, t-c[idx]);
+        
+        dp[t][idx]= min(tk, dt);
+        
+        return dp[t][idx];
     }
 };
