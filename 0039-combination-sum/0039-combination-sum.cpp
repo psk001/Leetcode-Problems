7b1e8195e9c32f6413e1a09d4a0d09426078aef9
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2023-09-29 04:51:46 +0530

    Time: 20 ms (30.33%), Space: 23.4 MB (24.70%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& cd, int target) {
+        sort(cd.begin(), cd.end());
+        
+        set<vector<int>> s;
+        vector<int> p;
+        
+        ss(cd, s, p, 0, target);
+        
+        vector<vector<int>> res;
+        
+        for(auto i: s)
+            res.push_back(i);
+        
+        return res;
+        
+    }
+    
+    void ss(vector<int>& cd, set<vector<int>>& s, vector<int> p, int idx, int t){
+        if(t==0){
+            s.insert(p);
+            // return;
+        }
+        
+        if(idx==cd.size())
+            return;
+        
+        if(t<cd[idx])
+            return;
+        
+        ss(cd, s, p, idx+1, t);
+        p.push_back(cd[idx]);
+        ss(cd, s, p, idx, t-cd[idx]);
+                    
+    }
+};
